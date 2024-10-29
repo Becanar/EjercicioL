0c182c8953aa9526b992a29d4ab1394d55f47f36
Becanar <benatortubitarte@gmail.com>
2024-10-29 15:22:35 +0100

    Añado login fxml

--- src/main/java/com/example/ejerciciol/loginController.java
+++ src/main/java/com/example/ejerciciol/loginController.java
@@ -1,14 +1,18 @@
 package com.example.ejerciciol;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class loginController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void login(ActionEvent actionEvent) {
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.ejerciciol {
 
     opens com.example.ejerciciol to javafx.fxml;
     exports com.example.ejerciciol;
+    exports com.example.ejerciciol.app;
+    opens com.example.ejerciciol.app to javafx.fxml;
 }
