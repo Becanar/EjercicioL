96eb47b37d5a36d30e43c6f087773985c08f6dd5
Becanar <benatortubitarte@gmail.com>
2024-10-31 11:21:22 +0100

    Javadoc Generado

---
+++ target/reports/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.ejerciciol","l":"com.example.ejerciciol.app"},{"m":"com.example.ejerciciol","l":"com.example.ejerciciol.controladores"}];updateSearchResults();
